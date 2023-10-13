dcbbfd488804f7dc630ced1165fe23ddb10c26f5
Douglas Hill <drhill99@hotmail.com>
2023-10-13 11:36:18 -0600

    files removed and functions added to main

--- NotesManager/src/main/kotlin/Main.kt
+++ NotesManager/src/main/kotlin/Main.kt
@@ -6,19 +6,12 @@ import java.util.*
 
 
 fun createFile(fileName: String, directoryName: String) {
-//    val inputReader = Scanner(System.`in`)
     println("Creating a new file.\n")
-//    print("Enter a file name:")
-//    val fileName = readln()
-//    val filePath ="text_files/$fileName.txt"
-//    val file = File(filePath)
-//    writeToFile(fileName)
     var filePath = "$directoryName/$fileName.txt"
     filePath = filePath.trim()
     val file = File(filePath)
-    print("$fileName >> ")
+    print("$directoryName/$fileName >> ")
     val userTextInput = readln()
-
     try {
         // create writer pointing at the desired file
         val writer = BufferedWriter(FileWriter(file))
@@ -31,41 +24,31 @@ fun createFile(fileName: String, directoryName: String) {
         e.printStackTrace()
     }
     appendToFile(fileName, directoryName)
-
 }
 
 fun findWordInFile(fileName: String, wordToFind: String, directoryName: String) {
-//    println("finding word.....")
+
     if(getListOfAllFiles(directoryName, print=false).contains("$fileName.txt")){
         println("finding references to \"$wordToFind\".........")
         val fileAsStrings = readFileAsLines(fileName, directoryName)
-//      print("Enter word to find: ")
-//      val wordToFind: String = readlnOrNull() ?: return
-
+        var wordFound: Boolean = false
         outerLoop@ for ((lineNumber, line) in fileAsStrings.withIndex()) {
-//        println("Current line: $line") // Test code
-
             innerLoop@ for (i in line.indices) {
                 if (i + wordToFind.length > line.length) {
                     // Skip if the remaining characters are less than the word length
                     continue@outerLoop
                 }
-
                 val testWord = line.substring(i, i + wordToFind.length).lowercase()
-//            val remainingIndices = i + wordToFind.length // test code
-//            println("remaining indices: $remainingIndices") // test code
-//            val lineLength = line.length // test code
-//            println("line length: $lineLength") // test code
                 if ((i + wordToFind.length) < line.length) {
                     if (testWord == wordToFind && (i == 0 || line[i - 1] == ' ') && line[i + wordToFind.length] == ' ') {
-//                    println("Found $wordToFind")
+                        wordFound = true
                         println("   Line:${lineNumber + 1}   $line")
                         // break the inner loop if the word is found early in the line
                         break@innerLoop
                     }
                 } else {
                     if (testWord == wordToFind && (i == 0 || line[i - 1] == ' ')) {
-
+                        wordFound =true
                         println("Line: ${lineNumber + 1} $line")
                         // break the inner loop if the word is found early in the line
                         break@innerLoop
@@ -73,9 +56,23 @@ fun findWordInFile(fileName: String, wordToFind: String, directoryName: String) 
                 }
             }
         }
+//        println("testword: $wordFound")
+        if(!wordFound){
+            println("No references to \"$wordToFind\" found.")
+        }
     } else {
         println("invalid input: file does not exist")
         return
+    }
+}
+
+fun findWordInDirectory(wordToFind: String, directoryName: String){
+    val listOfFilesInDir = getListOfAllFiles(directoryName, print = false)
+    for(file in listOfFilesInDir){
+        val fileExtensionIndex = file.indexOf('.')
+        val fileName = file.substring(0, fileExtensionIndex)
+        print("In $fileName...")
+        findWordInFile(fileName, wordToFind, directoryName);
     }
 
 }
@@ -100,8 +97,8 @@ fun appendToFile(fileName: String, directoryName: String){
         println("Invalid input: File does not exist")
         return
     }
-
 }
+
 fun getListOfAllFiles(directoryName: String, print: Boolean): List<String> {
     var filesInMemory: List<String> = emptyList()
     val absolutePath = Paths.get("").toAbsolutePath().toString()
@@ -134,6 +131,7 @@ fun printFile(fileName: String, directoryName: String) {
         println("     $line")
     }
 }
+
 // take user input string and remove commands and file names
 fun parseInput(userCommandInput: String): Triple<String, String, String> {
     fun findNumSpaces(userCommandInput: String): List<Int> {
@@ -183,32 +181,32 @@ fun createDirectory(directoryName: String) {
     }
 }
 fun menu(){
-
     var currentDirectory:String = "text_files" // default directory
     val menuOptions:String = "menu options:\n" +
-            "  grep \"file name\" \"word\" = search for a key word in a specified file\n" +
-            "    cd / \"directory name\" = change to specified direcory\n" +
-            "      mkdir / \"file name\" = create the specified directory\n" +
-            "        touch \"file name\" = create new file\n" +
-            "       append \"file name\" = add new lines of text to existing file\n" +
-            "           rm \"file name\" = delete the specified file\n" +
-            "                     more = print the contents of a file\n" +
-            "                       ls = print list of all file names\n" +
-            "                     quit = exit program\n" +
-            "                     help = display command options"
+            " mkdir as \"file name(one word)\" = create the specified directory\n" +
+            "         cd to \"directory name\" = change to specified direcory\n" +
+            "        find \"file name\" \"word\" = search for a key word in a specified file\n" +
+            "           findall indir \"word\" = search the active directory for a key word\n" +
+            "               read \"file name\" = print the contents of a file\n" +
+            "            newfile \"file name\" = create new file\n" +
+            "             append \"file name\" = add new lines of text to existing file\n" +
+            "             delete \"file name\" = delete the specified file\n" +
+            "                             ls = print list of all file names\n" +
+            "                           quit = exit program\n" +
+            "                           help = display command options"
 
     println(menuOptions)
     mainLoop@ while(true) {
-
         print("\n$currentDirectory/menu >> ")
         val userCommandString = readlnOrNull()?.trim()?.lowercase()?: break
         val(command, fileName, argument) = parseInput(userCommandString)
         when(command) {
-            "touch" -> createFile(fileName, currentDirectory)
-            "ls" -> getListOfAllFiles(currentDirectory, print=true)
+            "newfile" -> createFile(fileName, currentDirectory)
+            "listall" -> getListOfAllFiles(currentDirectory, print=true)
             "append" -> appendToFile(fileName, currentDirectory)
-            "more" -> printFile(fileName, currentDirectory)
-            "grep" -> findWordInFile(fileName, argument, currentDirectory)
+            "read" -> printFile(fileName, currentDirectory)
+            "find" -> findWordInFile(fileName, argument, currentDirectory)
+            "findall" -> findWordInDirectory(argument, currentDirectory)
             "rm" -> deleteFile(fileName)
             "mkdir" -> createDirectory(argument)
             "cd" -> currentDirectory = argument
@@ -217,7 +215,6 @@ fun menu(){
         }
     }
 }
-// TODO( implement the option to put a new file in any directory)
 fun main(args: Array<String>) {
 
     val textFilesDirectory = "text_files"
@@ -229,13 +226,13 @@ fun main(args: Array<String>) {
         createDirectory(textFilesDirectory)
     }
     menu()
+    // TODO( implement the option to put a new file in any directory) DONE
     //TODO( implement the opening and appending to a file) DONE
     //TODO( implement the opening and editing of a file) DONE
     //TODO( implement file reading) DONE
-    //TODO( add error handling)
+    //TODO( add error handling) MOSTLY DONE
     //TODO( hide the menu, add command to show the command options, a la linux command line) DONE
     //TODO( implement command parsing ie: print test7. parse out the command and the filename) DONE
-
     //TODO (research 'when' keyword) DONE
 
 
